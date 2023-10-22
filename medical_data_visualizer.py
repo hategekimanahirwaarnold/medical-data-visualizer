69775e575dfc12cafb68bcb06ada2cc9b6c93943
Hirwa Arnold <macbook@macbooks-MacBook-Pro.local>
2023-10-22 14:00:31 +0200

    Fix: cleaned data related to quartiles and ensured all tests pass

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -6,12 +6,6 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 # Suppress UserWarning warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-import ssl
-import urllib.request
-
-# Dsable ssl certificate verification
-ssl._create_default_https_context = ssl._create_unverified_context
-
 # begin code after disabling
 import pandas as pd
 import seaborn as sns
@@ -27,22 +21,17 @@ df['overweight'] = (df['weight'] / (df['height'] / 100) ** 2 > 25).astype(int)
 # Normalize data for 'cholesterol' and 'gluc'
 df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
 df['gluc'] = (df['gluc'] > 1).astype(int)
-
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
     df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
-    # print("The df_cat used to melt: \n", df_cat)
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature.
     grouped = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
-    gr = df_cat.groupby(['cardio', 'variable', 'value']).size()
-    # print("Grouped data up to size: \n",gr,"\n Grouped data up to reset_index()\n", grouped)
     # Draw the catplot with 'sns.catplot()'
     g = sns.catplot(x='variable', y='total', hue='value', col='cardio', data=grouped, kind='bar')
-    g.set_axis_labels('Variables', 'Total')
+    g.set_axis_labels('variable', 'total')
     g.set_titles('Cardio: {col_name}')
     # Get the figure for the output
-    # fig =  plt.show()
-    fig = plt
+    fig = plt.gcf()
     # Do not modify the next two lines
     fig.savefig('catplot.png')
     return fig
@@ -51,28 +40,29 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     df['id'] = df.index
-    columns = pd.Series(['id', 'age', 'gender', 'height', 'weight', 'ap_hi', 'ap_lo', 'cholesterol',
+    columns = pd.Series(['id', 'age', 'sex', 'height', 'weight', 'ap_hi', 'ap_lo', 'cholesterol',
        'gluc', 'smoke', 'alco', 'active', 'cardio', 'overweight'])
 
     # Clean the data
-    df_heat = df
-    df_heat.columns = columns
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) & 
+                (df['height'] >= df['height'].quantile(0.025)) &
+                (df['height'] <= df['height'].quantile(0.975)) &
+                (df['weight'] >= df['weight'].quantile(0.025)) &
+                (df['weight'] <= df['weight'].quantile(0.975))
+                ]
     # Calculate the correlation matrix
-    corr =df_heat.corr()
-
+    df_heat = df_heat[columns]
+    correlation =df_heat.corr()
     # Generate a mask for the upper triangle
-    mask = np.triu(np.ones_like(corr, dtype=bool))
-    corr = corr.where(~mask)
-
+    mask = np.triu(np.ones_like(correlation, dtype=bool))
+    corr = correlation.where(~mask)
 
     # Set up the matplotlib figure
     fig, ax = plt.subplots()
     
-
     # Draw the heatmap with 'sns.heatmap()'
-    sns.heatmap(data=corr, annot=True, fmt=".1f", vmax=1, vmin=-1)
+    sns.heatmap(data=corr, square=True, linewidths=0.5, annot=True, fmt="0.1f")
 
-    # plt.show()
     # Do not modify the next two lines
     fig.savefig('heatmap.png')
     return fig
